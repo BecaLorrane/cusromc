0b65c19e11bac48d009ad0de0226074cd698deab
Rebeca Robbo <rebeca.lqa@gmail.com>
2022-05-10 19:52:31 -0300

    Instanciação

--- src/main/java/com/robbo/cursomc/CursomcApplication.java
+++ src/main/java/com/robbo/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.robbo.cursomc;
 
+import com.robbo.cursomc.domain.Categoria;
+import com.robbo.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
 
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 =  new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
     }
 }
